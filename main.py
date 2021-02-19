e7d3381101a01b9919ff1673c022db8efe768d26
billel mokeddem <bilel.crysis3@gmail.com>
2021-02-19 18:14:45 +0100

    Create a dictionary of articles

--- main.py
+++ main.py
@@ -7,9 +7,28 @@ raw = open('.\Data\cacm.all', 'r')
 
 raw = raw.readlines()
 
+# create a dictionary of articles from the collections
+import re
 
+l = []
+k1 = 1
 
+collection = {}
 
+for line in raw:
+    if re.match('\.I \d+', line):
+        m = re.match('\.I (\d+)', line)
+        k2 = m.group(1)
+        
+    if k1 == k2:
+        l.append(line)
+    else: 
+        collection[int(k1)] = l
+        l = []
+        k1 = k2
+collection[int(k1)] = l
+
+print(collection)
 
 
 
